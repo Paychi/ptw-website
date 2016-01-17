c07cc13accf37c6fec0bc9f44b943d75be48d7e6
fjosuem <josue@montes.cc>
2016-01-17 15:50:23 -0600

    Título de "EDITAR COMUNIDAD" agregado

--- app/views/admon/editcomunidad.blade.php
+++ app/views/admon/editcomunidad.blade.php
@@ -7,7 +7,12 @@ Ad-Comunidades
 @section('content_admon')
 	{{ Form::open(array('url' => '/admin/editcomunidad','method'=>'post','class'=>'', 'name'=>'form_noticias','id'=>'msform')) }}
 		<div class="row text-right">
-			<a href='{{URL::to('/')}}/admin/comunidades' class="btn btn-danger"> X </a>
+			<div style="float:left; margin-left:2%;">
+				<label style="color:#337AB7; font-size:2em;" class="control-label">Editar Comunidad</label>			
+			</div>
+			<div>
+				<a href='{{URL::to('/')}}/admin/comunidades' class="btn btn-danger"> X </a>
+			</div>	
 		</div>
 
 		@if(Session::has('mensajeError'))
